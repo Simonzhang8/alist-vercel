ec4d099d8c85d38195fa1e794e8f3ef8865d3c2b
张先淼 <165749751+Simonzhang8@users.noreply.github.com>
2024-07-20 01:18:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 # 下载 Alist 二进制文件
 echo "Downloading Alist binary..."
-wget -O alist.tar.gz https://github.com/alist-org/alist/releases/download/v3.35.0/alist-linux-amd64.tar.gz
+curl -L -o alist.tar.gz https://github.com/alist-org/alist/releases/download/v3.35.0/alist-linux-amd64.tar.gz
 tar -zxvf alist.tar.gz
 
 
